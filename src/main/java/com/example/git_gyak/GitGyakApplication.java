386376b5d1db63a519e79f3a047e7f3257971753
Krisztina Pelei <krisztina.pelei@nttdata.com>
2023-03-18 15:46:32 +0100

    init

---
+++ src/main/java/com/example/git_gyak/GitGyakApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_gyak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitGyakApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GitGyakApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/git_gyak/GitGyakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_gyak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitGyakApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
